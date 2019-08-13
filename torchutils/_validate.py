2a7c5569ef005f1aed45b654282803ec58a7679d
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2019-08-12 23:42:56 -0400

    Fixed parameter naming in _validate.py.

--- torchutils/_validate.py
+++ torchutils/_validate.py
@@ -1,8 +1,8 @@
 import torch.nn as _nn
 import torch.optim as _optim
 
-def _validate_param(val, name, paramType):
-    supportedTypes = {
+def _validate_param(val, name, param_type):
+    supported_types = {
         'int': int,
         'str': str,
         'float': float,
@@ -11,14 +11,14 @@ def _validate_param(val, name, paramType):
         'scheduler': [_optim.lr_scheduler._LRScheduler,
                     _optim.lr_scheduler.ReduceLROnPlateau]
         }
-    if paramType not in supportedTypes:
-        raise TypeError(('INTERNAL ERROR: paramType must be one of {}, but got {}. '
-                    'Please raise an issue on github.').format(supportedTypes, type(val)))
-    curType = supportedTypes[paramType]
-    if not isinstance(curType, list):
-        curType = [curType]
-    for ct in curType:
+    if param_type not in supported_types:
+        raise TypeError(('INTERNAL ERROR: param_type must be one of {}, but got {}. '
+                    'Please raise an issue on github.').format(supported_types, type(val)))
+    cur_type = supported_types[param_type]
+    if not isinstance(cur_type, list):
+        cur_type = [cur_type]
+    for ct in cur_type:
         if isinstance(val, ct):
             return
-    typeStr = str(curType[0])
-    raise TypeError('{} must be of type {}, but got {}.'.format(name, typeStr, type(val)))
+    type_str = str(cur_type[0])
+    raise TypeError('{} must be of type {}, but got {}.'.format(name, type_str, type(val)))
