da4a9e2e78f26980d6929680adff4edee16647b0
Dhananjai Sharma <31407075+dhananjaisharma10@users.noreply.github.com>
2019-08-12 21:37:01 -0400

    Function for counting trainable parameters

--- torchutils/models.py
+++ torchutils/models.py
@@ -1,6 +1,6 @@
 from ._validate import _validate_param
 
-__all__ = ['get_model_param_count']
+__all__ = ['get_model_param_count', 'get_trainable_param_count']
 
 def get_model_param_count(model):
     """
@@ -24,4 +24,28 @@ def get_model_param_count(model):
         if len(p.size()) > 1:
             val *= p.size(1)
         param_count += val
+    return param_count
+
+
+def get_trainable_param_count(model):
+    """
+    Count total trainable parameters in the PyTorch model.
+    Parameters
+    ----------
+    model : nn.Module
+        PyTorch model.
+    Returns
+    -------
+    int
+        Number of trainable parameters in the model.
+    """
+
+    _validate_param(model, 'model', 'model')
+    param_count = 0
+    for p in model.parameters():
+        if p.requires_grad:
+            val = p.size(0)
+            if len(p.size()) > 1:
+                val *= p.size(1)
+            param_count += val
     return param_count
