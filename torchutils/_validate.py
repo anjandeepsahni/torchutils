c5d0af1496ed6bc3a02e7fab72c0440394f62e7d
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2019-08-14 02:51:41 -0400

    Added support for model summary and flops.

--- torchutils/_validate.py
+++ torchutils/_validate.py
@@ -6,22 +6,27 @@ def _validate_param(val, name, param_type):
     supported_types = {
         'int': int,
         'str': str,
+        'list': list,
         'float': float,
+        'tuple': tuple,
         'model': _nn.Module,
         'optimizer': _optim.Optimizer,
         'scheduler': [_optim.lr_scheduler._LRScheduler,
                       _optim.lr_scheduler.ReduceLROnPlateau]
         }
-    if param_type not in supported_types:
-        raise TypeError(('INTERNAL ERROR: param_type must be one of {}, but '
-                         'got {}. Please raise an issue on github.').format(
-                         supported_types, type(val)))
-    cur_type = supported_types[param_type]
-    if not isinstance(cur_type, list):
-        cur_type = [cur_type]
-    for ct in cur_type:
-        if isinstance(val, ct):
-            return
+    if not isinstance(param_type, list):
+        param_type = [param_type]
+    for pt in param_type:
+        if pt not in supported_types:
+            raise TypeError(('INTERNAL ERROR: param_type must be one of {}, '
+                             'but got {}. Please raise an issue on '
+                             'github.').format(supported_types, type(val)))
+        cur_type = supported_types[pt]
+        if not isinstance(cur_type, list):
+            cur_type = [cur_type]
+        for ct in cur_type:
+            if isinstance(val, ct):
+                return
     type_str = str(cur_type[0])
     raise TypeError('{} must be of type {}, but got {}.'.format(
                     name, type_str, type(val)))
