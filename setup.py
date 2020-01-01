e10d3a05896dab2bee2e957f3045c9dd8655bea4
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2020-01-01 08:00:18 +0530

    Excluded test files from pypi package.

--- setup.py
+++ setup.py
@@ -7,20 +7,14 @@ with open("torchutils/_version.py") as fp:
     torchutils_version = fp.read().strip().split('__version__ = ')[1][1:-1]
 
 setuptools.setup(
-     name='torchutils',
-     version=torchutils_version,
-     author="Anjandeep Singh Sahni",
-     author_email="sahni.anjandeep@gmail.com",
-     description="PyTorch utility functions.",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/anjandeepsahni/pytorch_utils",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
-     keywords='machine-learning deep-learning pytorch neuralnetwork',
-     license='MIT'
- )
+    name='torchutils', version=torchutils_version,
+    author="Anjandeep Singh Sahni", author_email="sahni.anjandeep@gmail.com",
+    description="PyTorch utility functions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anjandeepsahni/torchutils.git", classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], keywords='machine-learning deep-learning pytorch neuralnetwork',
+    license='MIT')
