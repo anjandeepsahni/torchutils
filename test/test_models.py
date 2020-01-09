198abf2e3bbf190d95edcbbf5cc8175b15031498
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2020-01-09 19:24:21 +0530

    Added test coverage for softmax and relu flops.

--- test/test_models.py
+++ test/test_models.py
@@ -1,9 +1,47 @@
 import unittest as _unittest
 
 import torch as _torch
+import torch.nn as _nn
 import torchvision as _torchvision
 
 import torchutils as _tu
+
+
+class _SampleNetwork(_nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(_SampleNetwork, self).__init__()
+        self.features = _nn.Sequential(
+            _nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            _nn.ReLU(inplace=True),
+            _nn.BatchNorm2d(64),
+            _nn.AvgPool2d(kernel_size=3, stride=2),
+            _nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            _nn.LeakyReLU(inplace=True),
+            _nn.MaxPool2d(kernel_size=3, stride=2),
+            _nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            _nn.ReLU(inplace=True),
+            _nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            _nn.ReLU(inplace=True),
+            _nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            _nn.ReLU(inplace=True),
+            _nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = _nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = _nn.Sequential(_nn.Dropout(),
+                                         _nn.Linear(256 * 6 * 6, 4096),
+                                         _nn.LeakyReLU(inplace=True),
+                                         _nn.Dropout(), _nn.Linear(4096, 4096),
+                                         _nn.LeakyReLU(inplace=True),
+                                         _nn.Linear(4096, num_classes),
+                                         _nn.Softmax(-1))
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = _torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
 
 class _TestModels(_unittest.TestCase):
@@ -20,9 +58,9 @@ class _TestModels(_unittest.TestCase):
         self.assertEqual(nontrainable_params, 0)
 
     def test_get_model_flops(self):
-        model = _torchvision.models.alexnet()
-        alexnet_flops = 773304664
-        alexnet_mflops = 773.3
+        model = _SampleNetwork()
+        alexnet_flops = 774416847
+        alexnet_mflops = 774.44
         alexnet_gflops = 0.77
         flops = _tu.get_model_flops(model, _torch.rand((1, 3, 224, 224)))
         mflops = _tu.get_model_flops(model, _torch.rand((1, 3, 224, 224)),
